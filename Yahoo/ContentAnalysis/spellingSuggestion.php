8e79d0ba07e57c639d0eb04845aa6969a65233c2
Martin Jansen <mj@php.net>
2006-10-02 12:53:34 +0000

    * Switched to Apache License, Version 2.0.
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Yahoo/trunk@220914 c90b9560-bf6c-de11-be94-00142212c4b1

--- Yahoo/ContentAnalysis/spellingSuggestion.php
+++ Yahoo/ContentAnalysis/spellingSuggestion.php
@@ -4,19 +4,25 @@
 /**
  * Spelling Suggestion service
  *
- * PHP version 5
+ * Copyright 2005-2006 Martin Jansen
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * @category   Services
  * @package    Services_Yahoo
  * @author     Martin Jansen <mj@php.net>
- * @copyright  2005 Martin Jansen
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @copyright  2005-2006 Martin Jansen
+ * @license    http://www.apache.org/licenses/LICENSE-2.0  Apache License, Version 2.0
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/Services_Yahoo
  */
@@ -32,8 +38,8 @@ require_once "AbstractContentAnalysis.php";
  * @category   Services
  * @package    Services_Yahoo
  * @author     Martin Jansen <mj@php.net>
- * @copyright  2005 Martin Jansen
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @copyright  2005-2006 Martin Jansen
+ * @license    http://www.apache.org/licenses/LICENSE-2.0  Apache License, Version 2.0
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/Services_Yahoo
  * @link       http://developer.yahoo.net/content/V1/termExtraction.html
